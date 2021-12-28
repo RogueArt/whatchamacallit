55b59602f98b8f55c874be0d5926ac627386422e
RogueArt <altoraries@gmail.com>
2021-12-27 21:30:26 -0800

    Initialized project structure for the frontend

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to WordDiff!</h1>
+            <p>
+                To learn more about this project, click
+                <a href="https://github.com/RogueArt/word-diff">here</a>
+            </p>
+        </div>
+    )
+}
+
+export default App
