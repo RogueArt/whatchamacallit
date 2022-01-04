0214137b3c5bdc0ab323bd087c878fb0297bc53f
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2022-01-03 23:00:31 -0600

    add env variables to read uri and restrcture db management

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,45 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-// Set up connection to Mongo
-use mongodb::{bson::doc, options::ClientOptions, Client};
-use std::env;
+use ::rocket::State;
+use mongodb::{bson::doc, Client, Database};
+
+// pool struct
+struct PoolDb {
+    db: Database,
+}
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello from map diff!"
+    "Hello from Rust"
 }
 
-async fn mongo_connect() -> mongodb::error::Result<()> {
-    // Parse your connection string into an options struct
-    let mut client_options =
-        ClientOptions::parse("mongodb+srv://<username>:<password>@<cluster-url>/test?w=majority")
-            .await?;
-    // Manually set an option
-    client_options.app_name = Some("Rust Demo".to_string());
-    // Get a handle to the cluster
-    let client = Client::with_options(client_options)?;
-    // Ping the server to see if you can connect to the cluster
-    client
-        .database("admin")
-        .run_command(doc! {"ping": 1}, None)
-        .await?;
-    println!("Connected successfully.");
-    // List the names of the databases in that cluster
-    for db_name in client.list_database_names(None, None).await? {
-        println!("{}", db_name);
+// a call to api will test if you connect succesfully to database
+#[get("/api")]
+async fn api(pool: &State<PoolDb>) -> &'static str {
+    match pool.db.run_command(doc! {"ping": 1}, None).await {
+        Ok(_) => "successsss",
+        Err(_) => "F something gone wrong",
     }
-    Ok(())
 }
 
+fn env(inp: &str) -> String {
+    match std::env::var(inp) {
+        Ok(value) => value,
+        Err(_) => panic!("key {} not found", inp),
+    }
+}
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn rocket() -> _ {
+    let uri = env("MONGO_URI");
+    let db_str = env("MONGO_DATABASE");
+    // mongo connection
+    let client = Client::with_uri_str(&uri[..]).await.ok();
+    let db = client.unwrap().database(&db_str[..]);
+    //let collection = db.collection::<Document>("url");
+
+    // implement a pool to mongo db connection using manage
+    rocket::build()
+        .mount("/", routes![api, index])
+        .manage(PoolDb { db: db })
 }
